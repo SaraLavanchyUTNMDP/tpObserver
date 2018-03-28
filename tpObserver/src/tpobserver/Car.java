394578338ba5bc638a367875c7e89b063c22e3e1
SaraLavanchyUTNMDP <37505420+SaraLavanchyUTNMDP@users.noreply.github.com>
2018-03-28 16:14:58 -0300

    new version of rthe tpObserver
    the method setChanged() and NotifyObservers() should be call in the setState and not in Travel's method.

--- tpObserver/src/tpobserver/Car.java
+++ tpObserver/src/tpobserver/Car.java
@@ -28,17 +28,12 @@ public class Car extends Observable {
     public String travel(float distance){
         String posivilityOfTravel = "you will travel "+distance+" km";
         if( distance < this.independence){
-            CarState oldState = null;
-            try{
-                oldState = this.state.clone();
-            }catch(CloneNotSupportedException e){}
-            state.setOilLevel(state.getOilLevel()*this.decrement(distance));
-            state.setTirePresion(state.getTirePresion()+(this.decrement(distance)*100));
-            state.setWaterLevel(state.getWaterLevel()*this.decrement(distance));
+            
+            float oilLevel = state.getOilLevel()*this.decrement(distance);
+            float tirePresion = state.getTirePresion()+(this.decrement(distance)*100);
+            float waterLevel = state.getWaterLevel()*this.decrement(distance);
+            this.setCarState(oilLevel, tirePresion, waterLevel);
             this.independence=this.independence-distance;
-            setChanged();
-            notifyObservers(oldState);
-            clearChanged();
             
         }else{
             posivilityOfTravel = "you don't have the independence necesary "
@@ -60,6 +55,9 @@ public class Car extends Observable {
 
     public void setModel(String model) {
         this.model = model;
+        setChanged();
+        notifyObservers();
+        clearChanged();
     }
 
     public String getCarKey() {
@@ -68,6 +66,9 @@ public class Car extends Observable {
 
     public void setCarKey(String carKey) {
         this.carKey = carKey;
+        setChanged();
+        notifyObservers();
+        clearChanged();
     }
 
     public float getIndependence() {
@@ -76,10 +77,28 @@ public class Car extends Observable {
 
     public void setIndependence(float independence) {
         this.independence = independence;
+        setChanged();
+        notifyObservers();
+        clearChanged();
     }
 
     public CarState getState() {
         return state;
+    }
+    
+    public void setCarState(float oil, float presion, float water){
+        CarState oldState = null;
+        try{
+            oldState = this.state.clone();
+        }catch(CloneNotSupportedException e){}
+        
+        this.state.setOilLevel(oil);
+        this.state.setTirePresion(presion);
+        this.state.setWaterLevel(water);
+        
+        setChanged();
+        notifyObservers(oldState);
+        clearChanged();
     }
 
     @Override
